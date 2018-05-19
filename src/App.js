cbd630ea9b582205a3f091b7546e8a8319774dd9
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-19 13:27:39 +0000

    Add likebutton css

--- src/App.js
+++ src/App.js
@@ -14,30 +14,71 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Body text={this.props.text}/>
+        <LikeButton />
         <Footer />
       </div>
     );
   }
 }
 
-class Body extends Component {
+class LikeButton extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      text: ""
-    };
   }
 
-  submit(){
-    alert(this.state.text);
+  styles(){
+    return {
+      container: {
+        fontFamily: "helvetica, arial, 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif",
+        fontSize: 11
+      },
+      like: {
+        display: "inline-block,
+        background: "#3b5998",
+        padding: "0px 5px",
+        rderRadius: 2,
+        color: "#ffffff",
+        cursor: "pointer",
+        float: "left",
+        height: 20,
+        lineHeight: "20px"
+      },
+      likeHover: {
+        background: "#444"
+      },
+      counterBefore: {
+        display: "block",
+        float: "left",
+        width: 6,
+        height: 6,
+        background: "#fafafa",
+        marginLeft: "-12px",
+        borderRight: 10,
+        transform: "rotate(45deg)",
+        WebkitTransform: "rotate(45deg)",
+        marginTop: 6,
+        borderLeft: "1px solid #aaa",
+        borderBottom: "1px solid #aaa"
+      },
+      counter: {
+        display: "block",
+        background: "#fafafa",
+        boxSizing: "border-box",
+        border: "1px solid #aaa",
+        float: "left",
+        padding: "0px 8px",
+        borderRadius: 2,
+        marginLeft: 8,
+        height: 20,
+        lineHeight: "20px"
+      }
+    };
   }
 
   render() {
     return (
       <div>
-        <input type="text" value={this.state.text} onChange={(e) => this.setState({text: e.target.value})}/>
-        <button className="button" onClick={() => this.submit()}>{this.props.text}</button>
+        いいねボタン建設予定地
       </div>
     );
   }
